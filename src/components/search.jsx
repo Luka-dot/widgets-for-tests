4f19bdb344e18bd3a2bc70ecaffcb25ac1c09c1f
Luka-dot <79lukask@gmail.com>
2021-02-20 13:34:37 -0500

    api trottle implemented to delay calls

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,45 +1,54 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const Search = () => {
-  const [searchText, setSearchText] = useState('programing');
+  const [searchText, setSearchText] = useState("programing");
   const [result, setResult] = useState([]);
 
   useEffect(() => {
     const search = async () => {
-        const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-            params: {
-                action: 'query',
-                list: 'search',
-                origin: '*',
-                format: 'json',
-                srsearch: searchText
-            },
-        });
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: searchText,
+        },
+      });
 
-        setResult(data.query.search)
+      setResult(data.query.search);
     };
-    
-    search();
+
+    const timeoutId = setTimeout(() => {
+      if (searchText) {
+        search();
+      }
+    }, 800);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
   }, [searchText]);
 
   const renderResults = result.map((item) => {
     return (
       <div className="item" key={item.pageid}>
-      <div className="right floated content">
-        <a
-         href={`https://en.wikipedia.org?curid=${item.pageid}`}
-         className="ui button">Go</a>
-      </div>
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${item.pageid}`}
+            className="ui button"
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
-          <div className="header">
-            {item.title}
-          </div>
+          <div className="header">{item.title}</div>
           <span dangerouslySetInnerHTML={{ __html: item.snippet }}></span>
         </div>
       </div>
-    )
-  })
+    );
+  });
 
   return (
     <div>
@@ -53,9 +62,7 @@ const Search = () => {
           />
         </div>
       </div>
-      <div className="ui celled list">
-        {renderResults}
-      </div>
+      <div className="ui celled list">{renderResults}</div>
     </div>
   );
 };
